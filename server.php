c2c8c655fbe544284b86d0f3a5e338f9b225b808
Neko <cronhound@gmail.com>
2020-10-20 11:54:46 +1030

    add nette example to server.php

--- server.php
+++ server.php
@@ -3,11 +3,16 @@ use Mark\App;
 
 require 'vendor/autoload.php';
 
+$db = new Nette\Database\Connection('mysql:host=127.0.0.1;dbname=eroticat', 'root', '');
+
 $api = new App('http://0.0.0.0:6969');
 $api->count = 4; // process count
 
 $api->any('/', function ($requst) use ( $db ) {
-    return 'Hello world';
+    $result = $db->query('SELECT * FROM pomf_users');
+    $count = $result->getRowCount();
+
+    return "There are {$count} users in the database.";
 });
 
 $api->get('/hello/{name}', function ($requst, $name) {
